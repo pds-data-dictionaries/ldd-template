8cdd8c953ced72cc607325913a230b0af436a9cd
Trent Hare <thare@usgs.gov>
2022-05-06 08:48:08 -0700

    Update conf.py
    reversed changed such that docs will build again (wrong repo).

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Cartography Namespace'
+project = 'Example Namespace'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
