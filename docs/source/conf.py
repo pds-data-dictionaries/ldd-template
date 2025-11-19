02e886831b079547b98fb81aa64ca68599296184
Jordan Padams <33492486+jordanpadams@users.noreply.github.com>
2025-11-18 16:20:35 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Example Namespace'
+project = 'Foobar Namespace'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
